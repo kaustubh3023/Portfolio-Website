36bc3866082b260bd6707c4db7f3847f814da329
kaustubh3023 <kaustubhsharma231219@gmail.com>
2026-06-10 12:26:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const host = "127.0.0.1";
+const host = "0.0.0.0";
 const port = process.env.PORT || 3000;
 const publicDir = path.join(__dirname, "public");
 
